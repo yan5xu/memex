f3dada01674a9a909f2aba837dceebe5d3332abf
yan5xu <lueco.x@gmail.com>
2026-07-07 18:17:34 +0800

    fix(web): keep active type hover stable

---
+++ internal/web/dist/assets/swimlanesDiagram-G3AALYLV-DdWMTeuW.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-BR3b0Ptb.js";import{_ as a}from"./mermaid.core-Kqwv5QIl.js";import"./chunk-5VM5RSS4-D_l01XDd.js";import"./chunk-XXDRQBXY-DTUg16AW.js";import"./chunk-VR4S4FIN-BqN6M0sJ.js";import"./chunk-32BRIVSS-DOWWKML_.js";import"./channel-ChZ_y84j.js";import"./index-Db3H3Q8m.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
